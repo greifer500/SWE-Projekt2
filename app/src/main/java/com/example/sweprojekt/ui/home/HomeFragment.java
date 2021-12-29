950abbf8857125328d0ecf8425000b85e7e7e0c3
greifer500 <74004914+greifer500@users.noreply.github.com>
2021-12-29 18:27:32 +0100

    BaseCode

---
+++ app/src/main/java/com/example/sweprojekt/ui/home/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.example.sweprojekt.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.sweprojekt.R;
+import com.example.sweprojekt.databinding.FragmentHomeBinding;
+import com.example.sweprojekt.ui.dashboard.DashboardFragment;
+
+public class HomeFragment extends Fragment {
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
