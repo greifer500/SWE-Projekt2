e6d2d2994368e7f831c481c5ceca7e4e68e3bbef
greifer500 <74004914+greifer500@users.noreply.github.com>
2021-12-30 12:00:11 +0100

    Dummy QR Fragment hinzugefügt

---
+++ app/src/main/java/com/example/sweprojekt/ui/qrcodescanner/QRFragment.java
@@ -0,0 +1,38 @@
+package com.example.sweprojekt.ui.qrcodescanner;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sweprojekt.R;
+
+public class QRFragment extends Fragment {
+
+    private QRViewModel mViewModel;
+
+    public static QRFragment newInstance() {
+        return new QRFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.q_r_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(QRViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/sweprojekt/ui/qrcodescanner/QRViewModel.java
@@ -0,0 +1,7 @@
+package com.example.sweprojekt.ui.qrcodescanner;
+
+import androidx.lifecycle.ViewModel;
+
+public class QRViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
