596bbc7483c84f9ee98dfa0fc4e53471fe9e4ac3
greifer500 <74004914+greifer500@users.noreply.github.com>
2021-12-31 03:31:45 +0100

    ListFragment Button funktion gegeben. ListenFragmente für Rooms und Buildings erstellt, ohne logic (nicht final)

--- app/src/main/java/com/example/sweprojekt/ui/list/ListFragment.java
+++ app/src/main/java/com/example/sweprojekt/ui/list/ListFragment.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.sweprojekt.R;
 import com.example.sweprojekt.databinding.FragmentListBinding;
+import com.example.sweprojekt.ui.list.buildings.BuildingsListFragment;
+import com.example.sweprojekt.ui.list.rooms.RoomsListFragment;
 
 public class ListFragment extends Fragment {
     private FragmentListBinding binding;
@@ -18,7 +23,39 @@ public class ListFragment extends Fragment {
         binding = FragmentListBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        createButtonsList(root);
+
         return root;
+    }
+
+    private void createButtonsList(View v) {
+        if (true){
+            createFragmentManagerRooms();
+        }
+
+        Button btnRooms = v.findViewById(R.id.buttonRooms);
+        btnRooms.setOnClickListener(v1 -> createFragmentManagerRooms());
+
+        Button btnBuildings = v.findViewById(R.id.buttonBuilding);
+        btnBuildings.setOnClickListener(v1 -> createFragmentManagerBuildings());
+    }
+
+    private void createFragmentManagerRooms() {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerList, RoomsListFragment.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    private void createFragmentManagerBuildings() {
+        FragmentManager fragmentManager = getParentFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerList, BuildingsListFragment.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack(null)
+                .commit();
     }
 
     @Override
---
+++ app/src/main/java/com/example/sweprojekt/ui/list/rooms/RoomsListFragment.java
@@ -0,0 +1,27 @@
+package com.example.sweprojekt.ui.list.rooms;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sweprojekt.R;
+
+public class RoomsListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.rooms_list_fragment, container, false);
+
+    }
+
+}
+
