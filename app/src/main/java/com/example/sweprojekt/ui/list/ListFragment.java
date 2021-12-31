5c730a4601be0228b035513b498263c60bd271d5
greifer500 <74004914+greifer500@users.noreply.github.com>
2021-12-31 02:46:30 +0100

    Reset, Umbenennung von fragment Roomlist in List. Hinzufügen von Knöpfen und fragmentConttainer in xml

---
+++ app/src/main/java/com/example/sweprojekt/ui/list/ListFragment.java
@@ -0,0 +1,32 @@
+package com.example.sweprojekt.ui.list;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.sweprojekt.R;
+import com.example.sweprojekt.databinding.FragmentListBinding;
+
+public class ListFragment extends Fragment {
+    private FragmentListBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentListBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
