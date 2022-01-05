c549cffd0ef667451c36e5f8462cb7db2ddabfe6
greifer500 <74004914+greifer500@users.noreply.github.com>
2022-01-05 23:52:23 +0100

    Button hinzugefügt, leerer onClickListener

--- app/src/main/java/com/example/sweprojekt/ui/home/protocol/ProtocolActivity.java
+++ app/src/main/java/com/example/sweprojekt/ui/home/protocol/ProtocolActivity.java
@@ -3,6 +3,8 @@ package com.example.sweprojekt.ui.home.protocol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 
 import com.example.sweprojekt.R;
@@ -14,7 +16,15 @@ public class ProtocolActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protocol);
+
         TableLayout tLProtocol = findViewById(R.id.tableLayoutProtocol);
+        Button bntSaveProtocol = findViewById(R.id.buttonActivityProtocol);
+        bntSaveProtocol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
