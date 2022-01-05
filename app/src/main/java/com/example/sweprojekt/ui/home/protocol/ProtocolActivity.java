873c12b578b7696560d6013324db8acb83668b38
greifer500 <74004914+greifer500@users.noreply.github.com>
2022-01-05 21:01:52 +0100

    activity_protocol.xml erstellt Table hinzugefügt

--- app/src/main/java/com/example/sweprojekt/ui/home/protocol/ProtocolActivity.java
+++ app/src/main/java/com/example/sweprojekt/ui/home/protocol/ProtocolActivity.java
@@ -3,8 +3,10 @@ package com.example.sweprojekt.ui.home.protocol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 import com.example.sweprojekt.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class ProtocolActivity extends AppCompatActivity {
 
@@ -12,5 +14,7 @@ public class ProtocolActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protocol);
+        TableLayout tLProtocol = findViewById(R.id.tableLayoutProtocol);
+
     }
 }
